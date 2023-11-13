593596cdd830855a9a46b635582f96504cc4f3f7
Nitesh Reddy <7451291+knr1931@users.noreply.github.com>
2023-11-13 12:39:10 +0530

    Added Type Hints

---
+++ nweng/matrix/_typing.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+from collections.abc import Iterable
+from typing import  TypeAlias, Union
+
+MatrixElemTypes: TypeAlias = int | float
+MatrixInputData: TypeAlias = Iterable[MatrixElemTypes]
--- nweng/matrix/matrixobject.py
+++ nweng/matrix/matrixobject.py
@@ -1,11 +1,17 @@
 from __future__ import annotations
 
 import numpy as np
-from typing import Type
 from ._matypes import _allow__matypes
+from ._typing import (
+    MatrixElemTypes,
+    MatrixInputData
+)
 
 class Matrix(np.ndarray):
-    def __new__(cls: Type[Matrix],data: list) -> Matrix:
+    
+    _rows: int
+    _cols: int = 1
+    def __new__(cls: type[Matrix],data: MatrixInputData) -> Matrix:
         matrix_obj = np.asarray(data).view(cls)
         if matrix_obj.dtype not in _allow__matypes:
             raise TypeError("Matrix should only contain real or complex numbers as it's elements")
