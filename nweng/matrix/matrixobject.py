08350f6116704f3e48784e6eeea4d5fd06f57f8b
Nitesh Reddy <7451291+knr1931@users.noreply.github.com>
2023-11-13 16:33:20 +0530

    Added news attributes to Matrix class rows,col

--- nweng/matrix/matrixobject.py
+++ nweng/matrix/matrixobject.py
@@ -9,13 +9,30 @@ from ._typing import (
 
 class Matrix(np.ndarray):
     
-    _rows: int
-    _cols: int = 1
     def __new__(cls: type[Matrix],data: MatrixInputData) -> Matrix:
-        matrix_obj = np.asarray(data).view(cls)
-        if matrix_obj.dtype not in _allow__matypes:
-            raise TypeError("Matrix should only contain real or complex numbers as it's elements")
-        return matrix_obj
-
-    def determinant(self):
-        return _allow__matypes
+        try:
+            matrix_obj = np.asarray(data).view(cls)
+            if matrix_obj.dtype not in _allow__matypes:
+                raise TypeError("Matrix should only contain real or complex numbers as it's elements")
+            if matrix_obj.ndim > 2:
+                raise ValueError("Matrix has only 1 or 2 dimensions") 
+            return matrix_obj
+        except ValueError:
+            raise ValueError("Matrix has insufficient number of elements")
+    
+    """
+        Checks wheter the input is a vector or not
+    """
+    def isVector(self) -> bool:
+        return self.ndim == 1    
+    
+    def get_rows(self) -> int:
+        if self.isVector(): 
+            return 1
+        return self.shape[0]
+        
+    def get_cols(self) -> int:
+        if self.isVector():
+            return self.shape[0]
+        return self.shape[1]
+        
--- nweng/matrix/tests/test_matrixobject.py
+++ nweng/matrix/tests/test_matrixobject.py
@@ -1,9 +1,50 @@
 from nweng.matrix import Matrix
+import pytest
 
-TEST_MATRIX_INPUT_1D = [1,2,3]
+class TestClassMatrixCreation:
+    
+    test_int_input = [5,4,6]
+    test_float_input = [4.55454654, 5.8454, 58.5]
+    test_int_float_input = [[1,2,3],[4.5,7.5,3]]
+    test_input_three_dims = [[[1,4],[2,5],[3,6]]]
+    test_str_input1 = [1,'2','fgjd']
+    test_str_input2 = [[1,2,4],['ifd','jgk','5']]
+    test_str_input3 = [[1,'f'],['gh']]
+    
 
-def test_matrix_creation1d():
-    A = Matrix(TEST_MATRIX_INPUT_1D)
-    assert isinstance(A,Matrix)
+    def test_matrix_creation1(self):
+        A = Matrix(self.test_int_input)
+        assert isinstance(A,Matrix)
+        assert A.get_rows() == 1
+        assert A.get_cols() == 3
+    
+    def test_matrix_creation2(self):
+        A = Matrix(self.test_float_input)
+        assert isinstance(A,Matrix)
+        assert A.get_rows() == 1
+        assert A.get_cols() == 3
+
+        
+    def test_matrix_creation3(self):
+        A = Matrix(self.test_int_float_input)
+        assert isinstance(A,Matrix)
+        assert A.get_rows() == 2
+        assert A.get_cols() == 3
+
+    def test_matrix_do_not_accept_3dims(self):
+        with pytest.raises(ValueError):
+            Matrix(self.test_input_three_dims)
+
+    def test_does_not_accept_str1(self):   
+        with pytest.raises(TypeError):
+            Matrix(self.test_str_input1)
+    
+    def test_does_not_accept_str2(self):   
+        with pytest.raises(TypeError):
+            Matrix(self.test_str_input2)
+    
+    def test_does_not_accept_str3(self):   
+        with pytest.raises(ValueError):
+            Matrix(self.test_str_input3)
 
 
